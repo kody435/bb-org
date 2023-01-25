ce46dfbee7b21bd1095eea70033f4f370b8a7b0f
Cody <bittusmaths@gmail.com>
2023-01-25 21:37:54 +0530

    Everythings working fine

--- components/Home.jsx
+++ components/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
   const subjects = [{ id: 1, name: "Math" }, { id: 2, name: "Science" }, { id: 3, name: "English" }, { id: 4, name: "History" }, { id: 5, name: "Geography" }, { id: 6, name: "Computer Science" }, { id: 7, name: "Physics" }, { id: 8, name: "Chemistry" }, { id: 9, name: "Biology" }, { id: 10, name: "Economics" }, { id: 11, name: "Accounting" }, { id: 12, name: "Business" }, { id: 13, name: "Psychology" }]
   
   return (
-    <div className="lg:grid items-start flex flex-col lg:grid-cols-10 mt-10 ">
+    <div className="lg:grid items-start flex flex-col lg:grid-cols-10 mt-10  ">
 
       {/* Subjects */}
       <div className={styles.grid2}>
@@ -68,7 +68,7 @@ const Home = () => {
         <div className="">
         <div className="border-2 bg-gradient-to-l from-cyan-500 to-blue-500 border-gray-200 rounded-t-3xl py-10 mr-10 pl-5" >
           <h1 className="text-5xl bg-transparent text-white font-extrabold flex mb-10">Have Questions ?</h1>
-          <Link href="/Ask-Question" className="bg-black border-2 border-black text-white rounded-full py-3 px-8 font-bold hover:bg-white hover:text-black">ASK NOW!</Link>
+          <Link href="/Ask-Question" className="bg-black border-2 border-black hover:border-white text-white rounded-full py-3 px-8 font-bold hover:bg-white hover:text-black">ASK NOW!</Link>
           </div>
           
         <div className="border-2 border-y-0 border-gray-200 mr-10 ">
