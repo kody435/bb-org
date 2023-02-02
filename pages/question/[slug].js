8a0286cab94aadec183159fb5ebe4c2732221d0a
Cody <bittusmaths@gmail.com>
2023-02-02 19:04:07 +0530

    Looking great! Just added the frontend of answer

--- pages/question/[slug].js
+++ pages/question/[slug].js
@@ -1,6 +1,8 @@
 import SDK from "weavedb-node-client";
 // import WeaveDB from "weavedb-client";
 const contractTxId = "ALgvvJ7aq7JnGFzamDkBhHnd1NzoXWOcmdrOMHCC3sA";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 export async function getServerSideProps({ query }) {
   let db = new SDK({
@@ -24,12 +26,72 @@ export async function getServerSideProps({ query }) {
 }
 
 export default function Question({ question }) {
+
+  const answers = [
+    {
+      answer: "This is the answer",
+      vote: 4,
+    },
+    {
+      answer: "This is the answer",
+      vote: 10,
+    },
+    {
+      answer: "This is the answer",
+      vote: 6,
+    },
+  ]
+
   return (
     <div className="flex flex-col ml-10 text-black">
       <div className="text-4xl font-bold mt-10 mb-4">{question.title}</div>
-      <div className="text-2xl font-medium mb-20">{question.question}</div>
-      <div className="px-7 py-3 font-extrabold text-lg rounded-full text-white bg-gradient-to-r from-indigo-500 to-red-500 max-w-fit ">
-        Upvote : {question.vote}
+      <div className="text-2xl font-medium mb-10">{question.question}</div>
+      <div>
+        <h2 className="font-bold text-3xl mt-16 mb-12">Answers</h2>
+        <div className="ml-10">
+          {answers.map((answer) => (
+            <div className="flex flex-col text-black mb-12">
+              <div className="text-2xl font-medium mb-3">{answer.answer}</div>
+              <div className="px-7 py-3 font-light text-lg rounded-full text-white bg-gradient-to-r from-indigo-500 to-red-500 max-w-fit ">
+                Upvote : {answer.vote}
+              </div>
+              <br></br>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* ANSWER */}
+      <div>
+        <Popup
+          trigger={
+            <div className="px-6 py-3 text-xl font-bold bg-gradient-to-r from-cyan-500 to-blue-500 w-fit rounded-full mb-16 cursor-pointer">
+              have Answer?
+            </div>
+          }
+          modal
+          nested
+        >
+          {(close) => (
+            <div className="w-full flex justify-center flex-col mt-10">
+              <h2 className="mb-5 flex justify-center font-semibold text-2xl">
+                Enter your answer here
+              </h2>
+              <div className="w-full flex justify-center mb-5">
+                <textarea
+                  type="text"
+                  className="w-full mx-14 rounded-2xl px-4 py-2 bg-white border-2 border-gray-400 h-48"
+                  placeholder="Enter your answer here"
+                />
+              </div>
+              <div className="flex justify-center mt-5">
+                <div className="flex justify-center mb-10 w-fit px-7 py-3 rounded-3xl bg-gradient-to-l from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
+                  Submit
+                </div>
+              </div>
+            </div>
+          )}
+        </Popup>
       </div>
     </div>
   );
