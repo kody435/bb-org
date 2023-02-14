5d190c6cebf7f2f8395e76d3c48ba5f4b1616fb4
Cody <bittusmaths@gmail.com>
2023-02-14 18:41:38 +0530

    Unknown changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Home from "../components/Home";
 import LandingPage from "../components/LandingPage";
-import MyApp from "./_app";
 import _app from 'next/app';
 
 const Main = (props) => {
